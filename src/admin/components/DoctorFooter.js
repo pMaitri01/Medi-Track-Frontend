a57aa89a88629b1f9cabb119010b0d613608d762
kishanpancholi12 <kp.developer04@gmail.com>
2026-03-13 15:27:07 +0530

    dashboard, herder, footer file without comment

--- src/admin/components/DoctorFooter.js
+++ src/admin/components/DoctorFooter.js
@@ -1,39 +1,3 @@
-// import React from "react";
-
-// export default function DoctorFooter({ isCollapsed }) {
-//   // Dynamic values
-//   const sidebarWidth = isCollapsed ? "80px" : "100px";
-
-//   const styles = {
-//     footer: {
-//       marginLeft: sidebarWidth, // 🔥 Dynamic Margin
-//       background: "#ffffff",
-//       padding: "15px 30px",
-//       borderTop: "1px solid #e5e7eb",
-//       display: "flex",
-//       justifyContent: "center",
-//       alignItems: "center",
-//       position: "fixed",
-//       bottom: 0,
-//       width: `calc(100% - ${sidebarWidth})`, // 🔥 Dynamic Width
-//       transition: "all 0.3s ease", // Smooth transition
-//     },
-//     center: {
-//       color: "#64748b",
-//       fontSize: "14px",
-//       textAlign: "center"
-//     }
-//   };
-
-//   return (
-//     <div style={styles.footer}>
-//       <div style={styles.center}>
-//         © {new Date().getFullYear()} MediTrack. All rights reserved.
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 
 export default function DoctorFooter({ open }) {
