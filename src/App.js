e9fb5fdf127330f3ade0c46cfbd14f10eabd9e88
Maitri <maitrip306@gmail>
2026-03-18 12:25:17 +0530

    added patientlist routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PatientOtp from './patient/pages/PatientOtp';
 import PatientResetPwd from './patient/pages/PatientResetPwd';
 import PatientRegistration from './patient/pages/PatientRegistration';
 import PatientHome from './patient/pages/PatientHome';
+import PatientList from './patient/pages/PatientList';
 import DoctorLogin from './admin/pages/DoctorLogin';
 import DoctorSignUp from './admin/pages/DoctorSignUp';
 import DoctorDashboard from './admin/pages/DoctorDashboard';
@@ -23,12 +24,24 @@ function App() {
         <Route path='/PatientResetPwd' element={<PatientResetPwd/>} />
         <Route path='/PatientRegistration' element={<PatientRegistration/>}/>
         <Route path='/PatientHome' element={<PatientHome/>}/>
+        <Route path='/PatientList' element={<PatientList/>}/>
         <Route path='/DoctorLogin' element={<DoctorLogin/>}/>
         <Route path='/DoctorSignUp' element={<DoctorSignUp/>}/> 
         <Route path='/DoctorDashboard' element={<DoctorDashboard/>}/>
 
       </Routes>
-      <ToastContainer />
+      <ToastContainer 
+        position="top-center" // This moves it to the top middle
+        autoClose={10000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </BrowserRouter>
   );
 }
