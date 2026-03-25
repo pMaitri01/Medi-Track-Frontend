86c213343b1e35503434451d482e685de5950f60
Sanjana <sanjanamali0903@gmail.com>
2026-03-25 17:08:28 +0530

    remove

--- src/patient/components/Navbar.js
+++ src/patient/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 // import { FaBell } from "react-icons/fa";
 import { FaBell, FaUserCircle,FaChevronDown } from "react-icons/fa";
 import "./Navbar.css";
