58e7a13db8ef667aeac6aa1e3451a4ff92c34504
Maitri <maitrip306@gmail.com>
2026-03-31 11:04:41 +0530

    admin - PMS

--- src/admin/pages/PatientManagement.js
+++ src/admin/pages/PatientManagement.js
@@ -36,7 +36,7 @@ const PatientManagement = () => {
       try {
         const token = localStorage.getItem("adminToken");
         const res = await fetch(
-          `${process.env.REACT_APP_API_URL}/api/patient/getPatients`,
+          `${process.env.REACT_APP_API_URL}/api/patient`,
           {
             method: "GET",
             headers: {
